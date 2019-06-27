ced4f9043fd54547a78875b5a278fad6943a25dd
SOUNISH NATH <flock.sinasini@gmail.com>
2019-06-27 08:50:08 +0530

    db err

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -4,7 +4,5 @@ class Notice < ApplicationRecord
   has_attached_file :attachment  #styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   #validates_attachment_content_type 
  validates_attachment :attachment, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
-  
-  # do_not_validate_attachment_file_type :attachment
 
 end
